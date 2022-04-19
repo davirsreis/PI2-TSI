8ad46a98f3b22e06da93f32edcf4c3ca28a3b1d9
davirsreis <daviresende.2312@gmail.com>
2022-04-19 14:26:26 -0300

    Quarto módulo

--- perfis/views.py
+++ perfis/views.py
@@ -7,11 +7,5 @@ def index(request):
 
 def exibir(request, perfil_id):
 
-    perfil = Perfil()
-
-    if perfil_id == 1:
-        perfil = Perfil('Fábio Henrique', 'fabio.oliveira@ifb.edu.br', '222222', 'IFB')
-    if perfil_id == 2:
-        perfil = Perfil('Davi Resende', 'davi.reis@estudante.ifb.edu.br', '4002-8922', 'IFB')
-
-    return render(request, 'perfil.html', {'perfil' : perfil})
+    perfil = Perfil.objects.get(id=perfil_id)
+    return render(request, 'perfil.html', { "perfil" : perfil})
