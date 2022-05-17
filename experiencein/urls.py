3f4f04fbff5fc28f421c14d099de91dbd62ab25f
davirsreis <daviresende.2312@gmail.com>
2022-05-17 11:57:24 -0300

    Módulo 10

--- experiencein/urls.py
+++ experiencein/urls.py
@@ -29,5 +29,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/',admin.site.urls), 
-    path('', include('perfis.urls'))
+    path('', include('perfis.urls')),
+    path('', include('usuarios.urls'))
 ]
---
+++ usuarios/urls.py
@@ -0,0 +1,8 @@
+# experiencein/usuarios/urls.py 
+
+from django.urls import path
+from usuarios.views import RegistrarUsuarioView
+
+urlpatterns = [
+    path('registrar/', RegistrarUsuarioView.as_view(), name='registrar')
+]
