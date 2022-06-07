167657e8d4e7f01d669a76a252b1f829677a0714
davirsreis <daviresende.2312@gmail.com>
2022-06-07 13:34:15 -0300

    Módulo 12 completo

--- perfis/views.py
+++ perfis/views.py
@@ -4,10 +4,20 @@ from perfis.models import Perfil, Convite
 from django.shortcuts import redirect
 
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import permission_required
 
 @login_required
 def index(request):
-    return render(request, 'index.html', {'perfis':Perfil.objects.all(), 'perfil_logado': get_perfil_logado(request)})
+    print(request.user.username) #novo
+    print(request.user.email) #novo
+    print(request.user.has_perm('perfis.add_convite')) #novo
+
+    if request.user.has_perm('perfis.add_convite')==True:
+        print('O usuário possui permissão')
+    else:
+        print('O usuário não possui permissão')
+
+    return render(request,'index.html',{'perfis': Perfil.objects.all(), 'perfil_logado' : get_perfil_logado(request) })
 
 @login_required
 def exibir(request, perfil_id):
@@ -18,6 +28,11 @@ def exibir(request, perfil_id):
 
     return render(request, 'perfil.html', {'perfil' : perfil, 'perfil_logado' : get_perfil_logado(request), 'ja_e_contato' : ja_e_contato})
 
+#Comando para adicionar permissão a algum usuário
+#>>> usuario = User.objects.get(username="Davi Resende")
+#>>> perm = Permission.objects.get(codename="add_convite")>>> usuario.user_permissions.add(perm)
+#>>> usuario.save()
+@permission_required('perfis.add_convite', raise_exception=True)
 @login_required
 def convidar(request, perfil_id):
 
